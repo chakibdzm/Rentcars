a5161fab1f3dfc4718b066553465d98c5a03edc0
oihid <c.ouchene@esi-sba.dz>
2022-10-14 15:48:57 +0100

    adding signin

---
+++ lib/Screens/UserScreens/SignIn.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:rentcar/size_config.dart';
+class Signinscreen extends StatefulWidget {
+  const Signinscreen({Key? key}) : super(key: key);
+
+  @override
+  State<Signinscreen> createState() => _SigninscreenState();
+}
+
+class _SigninscreenState extends State<Signinscreen> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return SafeArea(child: Scaffold
+      (
+
+    ));
+  }
+}
